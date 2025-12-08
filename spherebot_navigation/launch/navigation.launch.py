c7fdda5fb80d2e622594bc47c7fc49a19848fd76
Faseeh <faseehmohammed@outlook.com>
2025-12-09 00:09:50 +0400

    updated nav2 params + working nav2

---
+++ spherebot_navigation/launch/navigation.launch.py
@@ -0,0 +1,47 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_nav = 'spherebot_navigation'
+    pkg_nav2_bringup = 'nav2_bringup'
+
+    # --- Paths ---
+    # 1. The Map File
+    map_file = os.path.join(get_package_share_directory(pkg_nav), 'maps', 'assessment_map.yaml')
+    
+    # 2. The Params File
+    params_file = os.path.join(get_package_share_directory(pkg_nav), 'config', 'nav2_params.yaml')
+
+    # --- Launch Nav2 Bringup ---
+    # This launches everything: AMCL, Planner, Controller, BT Navigator, etc.
+    nav2_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory(pkg_nav2_bringup), 'launch', 'bringup_launch.py')
+        ),
+        launch_arguments={
+            'map': map_file,
+            'params_file': params_file,
+            'use_sim_time': 'true',
+            'autostart': 'true'  # Automatically transitions the nodes to Active state
+        }.items()
+    )
+
+    # --- RViz ---
+    # Launch RViz with the standard Nav2 configuration
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory(pkg_nav2_bringup), 'rviz', 'nav2_default_view.rviz')]
+    )
+
+    return LaunchDescription([
+        nav2_bringup,
+        rviz
+    ])
---
+++ spherebot_navigation/setup.py
@@ -0,0 +1,37 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'spherebot_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        # Install the package.xml (Standard)
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        
+        # [CRITICAL] Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        
+        # [CRITICAL] Install config files (Nav2 params)
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        
+        # [CRITICAL] Install map files (.yaml and .pgm)
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='Navigation configuration for spherebot',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
